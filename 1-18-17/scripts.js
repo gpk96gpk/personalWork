ed9716825430798940d5210c4ea2652b3b833fb3
gpk96gpk <gpk96gpk@gmail.com>
2017-01-18 14:42:15 +0100

    OG

---
+++ 1-18-17/scripts.js
@@ -0,0 +1,6 @@
+$('#cat').click(
+function(){
+    console.log ($('#cat'));
+    $(".conatainer").html('<img id="cat" src="https://cdn.pixabay.com/photo/2014/03/29/09/17/cat-300572_1280.jpg" alt="cat" title="cat"/>');
+}
+);
